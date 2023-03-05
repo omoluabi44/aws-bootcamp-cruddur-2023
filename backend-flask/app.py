667a3f256fb6dc7eda6f4f2be2409955886d8100
omoluabi44 <emmanuelogunleye441999@gmail.com>
2023-03-05 15:06:27 +0000

    implement cloudwatch log

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -31,6 +31,20 @@ import rollbar
 import rollbar.contrib.flask
 from flask import got_request_exception
 
+#CLOUDWATCH
+import watchtower
+import logging
+from time import strftime
+
+# Configuring Logger to Use CloudWatch
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
+LOGGER.info("homeActivities")
+
 #HONEYCOMB
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
@@ -63,6 +77,13 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+#CLOUDWATCH LOG
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
 #ROLLBAR
 rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
 @app.before_first_request
@@ -123,7 +144,7 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  data = HomeActivities.run()
+  data = HomeActivities.run(logger=LOGGER)
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
