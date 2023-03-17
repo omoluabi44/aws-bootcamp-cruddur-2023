21e4e65c20e13ee8b7af97f0892ef66c2dc08d06
omoluabi12 emman <ogunleyeemmanuel441999@gmail.com>
2023-03-17 20:57:18 +0000

    updaate

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
+from lib.db import pool
+
 tracer = trace.get_tracer("home.activities")
 class HomeActivities:
   def run(logger):
