4e04ad57723073e9eae2912ed1f39c049a011ec2
omoluabi12 emman <ogunleyeemmanuel441999@gmail.com>
2023-03-19 21:36:15 +0000

    update on home activity

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -2,19 +2,27 @@ from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 from lib.db import pool
 
-tracer = trace.get_tracer("home.activities")
+# tracer = trace.get_tracer("home.activities")
 class HomeActivities:
-  def run(cognito_user_id=None):
-    print("HOME ACTIVITY")
-    #logger.info("homeActivities")
-    with tracer.start_as_current_span("home.activities.mock.data"):
-      now = datetime.now(timezone.utc).astimezone()
+    def run(cognito_user_id=None):
+        print("HOME ACTIVITY")
+        #logger.info("homeActivities")
+        # with tracer.start_as_current_span("home.activities.mock.data"):
+        # now = datetime.now(timezone.utc).astimezone()
+        sql = """
+            SELECT * from activities   
+        """
 
-      with pool.connection() as conn:
-        with conn.cursor() as cur:
-          cur.execute(sql)
-          # this will return a tuple
-          # the first field being the data
-          json = cur.fetchone()
-      return json[0]
-      return results
+        with pool.connection() as conn:
+          with conn.cursor() as cur:
+              cur.execute(sql)
+              # this will return a tuple
+              # the first field being the data
+              json = cur.fetchone()
+
+        print("=3---------")
+        print("==---------")   
+        for record in cur:
+          print(record)
+        print(json)           
+        return json[0]
