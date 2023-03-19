7d8cc0e88805ea24b0568ef3b38ec8833c9a0e53
omoluabi12 emman <ogunleyeemmanuel441999@gmail.com>
2023-03-19 15:47:07 +0000

    corrected some wrong code also work on home activ.

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -4,47 +4,17 @@ from lib.db import pool
 
 tracer = trace.get_tracer("home.activities")
 class HomeActivities:
-  def run(logger):
-    logger.info("homeActivities")
+  def run(cognito_user_id=None):
+    print("HOME ACTIVITY")
+    #logger.info("homeActivities")
     with tracer.start_as_current_span("home.activities.mock.data"):
       now = datetime.now(timezone.utc).astimezone()
-      results = [{
-        'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-        'handle':  'OGUNLEYE EMMANUEL',
-        'message': 'Cloud is fun! but fucking hard mehhh',
-        'created_at': (now - timedelta(days=2)).isoformat(),
-        'expires_at': (now + timedelta(days=5)).isoformat(),
-        'likes_count': 5,
-        'replies_count': 1,
-        'reposts_count': 0,
-        'replies': [{
-          'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
-          'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-          'handle':  'Worf',
-          'message': 'This post has no honor!',
-          'likes_count': 0,
-          'replies_count': 0,
-          'reposts_count': 0,
-          'created_at': (now - timedelta(days=2)).isoformat()
-        }],
-      },
-      {
-        'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
-        'handle':  'Worf',
-        'message': 'I am out of prune juice',
-        'created_at': (now - timedelta(days=7)).isoformat(),
-        'expires_at': (now + timedelta(days=9)).isoformat(),
-        'likes': 0,
-        'replies': []
-      },
-      {
-        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-        'handle':  'Garek',
-        'message': 'My dear doctor, I am just simple tailor',
-        'created_at': (now - timedelta(hours=1)).isoformat(),
-        'expires_at': (now + timedelta(hours=12)).isoformat(),
-        'likes': 0,
-        'replies': []
-      }
-      ]
-    return results
+
+      with pool.connection() as conn:
+        with conn.cursor() as cur:
+          cur.execute(sql)
+          # this will return a tuple
+          # the first field being the data
+          json = cur.fetchone()
+      return json[0]
+      return results
