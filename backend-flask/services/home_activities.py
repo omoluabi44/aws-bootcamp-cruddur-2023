e3760503b2845a2333ca471c721e3f549a2d88b3
omoluabi12 emman <ogunleyeemmanuel441999@gmail.com>
2023-03-20 08:31:40 +0000

    we have a query working

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
-from lib.db import pool
+from lib.db import pool, query_wrap_array
 
 # tracer = trace.get_tracer("home.activities")
 class HomeActivities:
@@ -9,20 +9,27 @@ class HomeActivities:
         #logger.info("homeActivities")
         # with tracer.start_as_current_span("home.activities.mock.data"):
         # now = datetime.now(timezone.utc).astimezone()
-        sql = """
-            SELECT * from activities   
-        """
+        sql = query_wrap_array( """
+           SELECT
+        activities.uuid,
+        users.display_name,
+        users.handle,
+        activities.message,
+        activities.replies_count,
+        activities.reposts_count,
+        activities.likes_count,
+        activities.reply_to_activity_uuid,
+        activities.expires_at,
+        activities.created_at
+      FROM public.activities
+      LEFT JOIN public.users ON users.uuid = activities.user_uuid
+      ORDER BY activities.created_at DESC  
+        """)
 
         with pool.connection() as conn:
-          with conn.cursor() as cur:
-              cur.execute(sql)
-              # this will return a tuple
-              # the first field being the data
-              json = cur.fetchone()
-
-        print("=3---------")
-        print("==---------")   
-        for record in cur:
-          print(record)
-        print(json)           
+         with conn.cursor() as cur:
+          cur.execute(sql)
+          # this will return a tuple
+          # the first field being the data
+          json = cur.fetchone()
         return json[0]
