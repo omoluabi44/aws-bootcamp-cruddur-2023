91c61c1ee64d734a781b6f0aa8d33264fe1fafd6
omoluabi44 <emmanuelogunleye441999@gmail.com>
2023-03-24 08:36:55 +0000

    update implementing on create and home

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -1,20 +1,24 @@
 from psycopg_pool import ConnectionPool
 import os
 
-def query_wrap_object(template):
-  sql = f"""
-  (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
-  {template}
-  ) object_row);
-  """
-  return sql
-def query_wrap_array(template):
-  sql = f"""
-  (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
-  {template}
-  ) array_row);
-  """
-  return sql
+
+
+
+class db:
+  def _init_(self):
+    self.init_pool()
+
+  def init_pool():
+    connection_url = os.getenv("CONNECTION_URL")
+    self.pool = ConnectionPool(connection_url)
+  def query_commit():
+    try:
+       conn = self.pool.connection()
+       cur = conn.cursor
+       cur.execute(sql)
+       con.commit()
+    except OperationalError as err:
+      self.print_sql_err(err)
 
 def print_sql_err(err):
     # get details about the exception
@@ -34,5 +38,47 @@ def print_sql_err(err):
     print ("pgerror:", err.pgerror)
     print ("pgcode:", err.pgcode, "\n")
 
-connection_url = os.getenv("CONNECTION_URL")
-pool = ConnectionPool(connection_url)
+def query_object():
+  with pool.connection() as conn:
+         with conn.cursor() as cur:
+          cur.execute(sql)
+          # this will return a tuple
+          # the first field being the data
+          json = cur.fetchone()
+        return json[0]
+
+def query_array():
+
+def query_wrap_object(template):
+  sql = f"""
+  (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
+  {template}
+  ) object_row);
+  """
+  return sql
+def query_wrap_array(template):
+  sql = f"""
+  (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
+  {template}
+  ) array_row);
+  """
+  return sql
+def print_sql_err(err):
+    # get details about the exception
+    err_type, err_obj, traceback = sys.exc_info()
+
+    # get the line number when exception occured
+    line_num = traceback.tb_lineno
+
+    # print the connect() error
+    print ("\npsycopg ERROR:", err, "on line number:", line_num)
+    print ("psycopg traceback:", traceback, "-- type:", err_type)
+
+    # psycopg2 extensions.Diagnostics object attribute
+    print ("\nextensions.Diagnostics:", err.diag)
+
+    # print the pgcode and pgerror exceptions
+    print ("pgerror:", err.pgerror)
+    print ("pgcode:", err.pgcode, "\n")
+
+db = db()
--- backend-flask/services/create_activity.py
+++ backend-flask/services/create_activity.py
@@ -1,6 +1,6 @@
 import uuid
 from datetime import datetime, timedelta, timezone
-from lib.db import pool, print_sql_err
+from lib.db import db
 class CreateActivity:
   def run(message, user_handle, ttl):
     model = {
@@ -67,10 +67,5 @@ class CreateActivity:
 
     )
     """
-    try:
-       conn = pool.connection()
-       cur = conn.cursor
-       cur.execute(sql)
-       con.commit()
-    except OperationalError as err:
-      print_sql_err(err)
+    query_commit(sql)
+  
